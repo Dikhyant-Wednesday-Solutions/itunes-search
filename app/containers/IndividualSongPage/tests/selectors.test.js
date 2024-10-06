ed3ce50ce50d84ab0460afac381308e3b57c0fb0
Wednesday Solutions <wednesdaysolutions@Wednesdays-MacBook-Pro.local>
2024-10-07 02:04:33 +0530

    feat: add test case for individual song selector

---
+++ app/containers/IndividualSongPage/tests/selectors.test.js
@@ -0,0 +1,41 @@
+import { selectIndividualSongPageDomain, selectSongData, selectSongError, selectSongId } from '../selectors';
+import { initialState } from '../reducer';
+import songData from './song.data.json';
+
+describe('SongsContainer selector tests', () => {
+  let mockedState;
+  let songId;
+  let songError;
+
+  beforeEach(() => {
+    songId = '1440833081';
+    songError = 'There was some error while fetching the repository details';
+
+    mockedState = {
+      individualSongPage: {
+        songId: songId,
+        songData: songData,
+        songError: songError
+      }
+    };
+  });
+  it('should select the songName', () => {
+    const songSelector = selectSongId();
+    expect(songSelector(mockedState)).toEqual(songId);
+  });
+
+  it('should select songData', () => {
+    const songDataSelector = selectSongData();
+    expect(songDataSelector(mockedState)).toEqual(songData);
+  });
+
+  it('should select the songError', () => {
+    const songErrorSelector = selectSongError();
+    expect(songErrorSelector(mockedState)).toEqual(songError);
+  });
+
+  it('should select the global state', () => {
+    const selector = selectIndividualSongPageDomain(initialState);
+    expect(selector).toEqual(initialState);
+  });
+});
